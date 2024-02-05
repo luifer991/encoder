5f419d91c9e03bf957bf0fa070bb83899fccb1c0
Jorge Morales <luifer991@gmail.com>
2024-02-05 12:46:37 -0500

    organize app code

--- app.js
+++ app.js
@@ -6,18 +6,12 @@ La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat"
 */
 
-function encode () {
-    // get input text
-    let inputContent = document.getElementById('txt-encode').value;
-    // get div
-    let newDiv = document.getElementById('group');
-    // create button 
-    let btn = document.createElement('button');
-    btn.textContent = 'Copiar';
-    btn.classList.add("copy-btn");
-    btn.addEventListener('click', copyButton);
+// text processing function
+
+function textProcesing (text) {
+
     // Enconding text
-    let originalString = inputContent.toLowerCase();
+    let originalString = text.toLowerCase();
     let newString = originalString.replace(/[aeiou]/g, (match) => {
         if (match === "a") return "ai";
         else if (match === "i") return "imes";
@@ -26,43 +20,71 @@ function encode () {
         else if(match === "u") return "ufat";
         return "";
 });
+    return newString;
+}
+
+// button function
+
+function createBtn () {
+    let btn = document.createElement('button');
+    let newDiv = document.getElementById('group');
+    btn.textContent = 'Copiar';
+    btn.classList.add("copy-btn");
+    btn.addEventListener('click', copyButton);
+    newDiv.appendChild(btn);
+}
+
+// encode funtion
+
+function encode () {
+    // get input text
+    let inputContent = document.getElementById('txt-encode').value;
+    // get div
+    let newDiv = document.getElementById('group');
+    // Enconding text
+    let newString = textProcesing(inputContent);
     // put the text into the div
     newDiv.innerHTML = `<p>${newString}</p>`;
     // adding class
     newDiv.classList.add('text-encoded');
-    // append the button to the div
-    newDiv.appendChild(btn);
+    // append button to the div
+    createBtn();
     return;
 }
 
+// Replace function
 
-function decode(){
-    // create button 
-    let btn = document.createElement('button');
-    btn.textContent = 'Copiar';
-    btn.classList.add("copy-btn");
-    btn.addEventListener('click', copyButton);
+function replaceText(text) {
+    let stringCode = text.replace(/ai/g, "a")
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
+
+    return stringCode;
+}
+
+// Decode Funtion
+
+function decode(){ 
     // get the input text    
     let inputEncoded = document.getElementById('txt-encode').value;
     let newDiv = document.getElementById('group');
     // replace the text value to Decode
-    let stringCode = inputEncoded.replace(/ai/g, "a")
-    .replace(/enter/g, "e")
-    .replace(/imes/g, "i")
-    .replace(/ober/g, "o")
-    .replace(/ufat/g, "u");
+    let stringCode = replaceText(inputEncoded);
     // put the decoding text into the div
     newDiv.innerHTML = `<p>${stringCode}</p>`;
     newDiv.classList.add('text-encoded');
-    newDiv.appendChild(btn);
+    createBtn();
     return;
-  }
+}
+
+// Copy to clipboard funtion
 
 function copyButton (){ 
     let newDiv = document.getElementById('group');
     navigator.clipboard.writeText(newDiv.firstChild.textContent);
     newDiv.lastChild.textContent = "Copiado";
     return;
-
 }
 
